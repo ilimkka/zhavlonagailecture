febb84d441f53c20184be344f9b712a679de02ea
ilimkka <ilimtazhibaev@gmail.com>
2024-09-16 11:47:11 +0600

    bam

--- firstproject/settings.py
+++ firstproject/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0uc)2x54iz*32dt)ya#gz-j!@)ips!$qnq24+lq^)2^5ti4b6s'
+SECRET_KEY = 'django-insecure-(14#)aa9=nyt3#81a-arp-&cp1m4+%qxoj$#e(^7*plaz84b!n'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
